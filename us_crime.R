483d248877457c34923e5c1ed2dce6d9adb5447b
Geoffery Mullings <geoffmullings@gmail.com>
2022-01-12 18:22:14 -0500

    Added headers to R script and added headers and readme files

--- us_crime.R
+++ us_crime.R
@@ -1,2 +1,10 @@
-setwd("~/R/us_crime")
+setwd("~/R/mit_ml/us_crime")
 .libPaths(c("C:/Users/geoff/Documents/R/win-library/3.6"))
+
+dataset <- read.delim("CommViolPredUnnormalizedData.txt", sep = ",", header=FALSE)
+filename <- "headers.csv" # Headers had to be created in Excel from attribute information in README
+headers <- read.csv(filename)
+colnames(dataset) = colnames(headers)
+
+dim(dataset)
+sapply(dataset, class)
